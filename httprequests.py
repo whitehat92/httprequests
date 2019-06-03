3dcbca3ba2642ddadcce468002b1eabc74a6ba30
whitehat92 <ze_correia93@hotmail.com>
2019-06-03 23:12:17 +0100

    Update httprequests.py

--- httprequests.py
+++ httprequests.py
@@ -3,9 +3,13 @@ import requests
 host = str(input("Enter domain or IP: "))
 send = requests.get("http://" + host + "/")
 pedido = send.status_code
-#add "http://" prefix
-
+cabeca = send.headers
 if pedido == 200:
     print("200 OK")
+    print(cabeca)
 if pedido == 400:
     print("400 NOT FOUND")
+if pedido == 403:
+    print("403 FORBIDDEN")
+if pedido == 301:
+    print ("301 FOUND")
